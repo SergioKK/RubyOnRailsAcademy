22537971ca9e9d590a67918f530834ad8dc5cf83
sergio <khotsenkos1993k@gmail.com>
2024-02-22 17:26:03 +0300

    Created models, controllers, views for User and LabReport. Imlemented User and LabReport CRUD

---
+++ app/controllers/lab_reports_controller.rb
@@ -0,0 +1,54 @@
+class LabReportsController < ApplicationController
+  def index
+    @reports = LabReport.all
+  end
+
+  def show
+    @report = LabReport.find(params[:id])
+    @user = @report.user
+  end
+
+  def new
+    @report = LabReport.new
+  end
+
+  def create
+    @report = LabReport.new(report_params)
+    if @report.save
+      redirect_to @report
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def edit
+    @report = LabReport.find(params[:id])
+  end
+
+  def update
+    @report = LabReport.find(params[:id])
+
+    if @report.update(report_params)
+      redirect_to @report
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @report = LabReport.find(params[:id])
+    @report.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  def add_mark
+    @report = LabReport.find(params[:id])
+  end
+
+  private
+  def report_params
+    params.require(:lab_report).permit(:title, :description, :grade, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :lab_reports
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/lab_reports_add_mark/:id", to: "lab_reports#add_mark", :as => "add_mark"
 end
