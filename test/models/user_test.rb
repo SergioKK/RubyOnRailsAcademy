4c93a8b389f1b42816e815b9d23ce5ad84587c95
sergio <khotsenkos1993k@gmail.com>
2024-02-28 16:08:51 +0300

    Starting tests. Configuring fixtures for DB

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without first name" do
+    user = User.new
+    assert_not user.save, "Save user without first name"
+  end
+
+  test "playing with fixtures" do
+    lab_report = users(:harry).lab_reports
+    assert_equal(lab_report.collect(&:title)[0], "LabReport1")
+  end
 end
